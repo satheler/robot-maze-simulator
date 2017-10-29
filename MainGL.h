d03e778f3c389e76cc44cdebadeccfa7fed9f2f8
Lucas Martins <lgatts@gmail.com>
2017-10-29 13:05:21 -0200

    File Browser funcionando não é 100%
    depois de clicar o explorador não some mas funciona :)

---
+++ MainGL.h
@@ -0,0 +1,11 @@
+#ifndef MAINGL_H
+#define MAINGL_H
+#include <iostream>
+using namespace std;
+
+class MainGL
+{
+    public:
+        static void runGL(string arquivo);
+};
+#endif
--- main.cpp
+++ main.cpp
@@ -1,37 +1,59 @@
-#include "Labirinto.h"
-#include "ArTur.h"
-#include "Robby.h"
-#include "GL.h"
+#include <gtk/gtk.h>
+#include "MainGL.h"
 
 using namespace std;
 
 int main()
 {
-    GL::init();
-    // Load the test maze
-    Maze* lab = new Labirinto();
-    lab->loadMaze("maze-pacman.txt");
+    gtk_init(NULL, NULL);
 
-    Point posIni = lab->getIniPos();
-    cout << "Starting pos: " << posIni.getX() << " , " << posIni.getY() << endl;
-    int maxSteps = 1000;
-    Robot* robo;
+    char *filename;
+    GtkWidget *window;
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
-    switch(lab->getRobot())
+    GtkWidget *dialog;
+
+    //criando o filtro para arquivos txt
+    GtkFileFilter *filter = gtk_file_filter_new();
+    gtk_file_filter_set_name(filter,"Maze txt");
+    gtk_file_filter_add_pattern(filter,"*.txt");
+
+    //criando o explorador de arquivos
+    dialog = gtk_file_chooser_dialog_new("Choose a file",
+                                         GTK_WINDOW(window),
+                                         GTK_FILE_CHOOSER_ACTION_OPEN,
+                                         GTK_STOCK_OK,
+                                         GTK_RESPONSE_OK,
+                                         GTK_STOCK_CANCEL,
+                                         GTK_RESPONSE_CANCEL,
+                                         NULL);
+
+    //adicionando o filtro no explorador
+    gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dialog),filter);
+
+    gtk_widget_show_all(dialog);
+
+    gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dialog), g_get_home_dir());
+    gint resp = gtk_dialog_run(GTK_DIALOG(dialog));
+
+    //verificando se foi clicado em ok ou se foi cancelada a seleção de arquivos
+    if(resp == GTK_RESPONSE_OK)
     {
-    case 1:
-        robo = new Robby(posIni, lab, maxSteps);
-        break;
-    case 2:
-        robo = new ArTur(posIni, lab, maxSteps);
-        break;
-    case 3:
-        cout << "Robo 3" << endl;
-        //robo = new T8000();
-        break;
+        filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+    }
+    else
+    {
+        g_print("You don't choose a file!\n");
     }
 
-    GL::setMazeRobot(lab, robo);
+    gtk_widget_destroy(dialog);
 
-    GL::loop();
+    //convertendo o caminho do arquivo para string
+    std::string filenameString(filename);
+
+    //executando a ação principal do openGL maze
+    MainGL::runGL(filenameString);
+
+    gtk_main();
+
 }
