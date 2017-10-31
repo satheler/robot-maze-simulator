adef763a87399331d616785e19e448bf2c485d91
¨Gustavo <gustavo.satheler@alunos.unipampa.edu.br>
2017-10-31 21:52:20 -0200

    Inicializado o T800

--- MainGL.cpp
+++ MainGL.cpp
@@ -1,6 +1,9 @@
 #include "Labirinto.h"
+
 #include "ArTur.h"
 #include "Robby.h"
+#include "T800.h"
+
 #include "GL.h"
 #include "MainGL.h"
 #include <iostream>
@@ -21,13 +24,17 @@ void MainGL::runGL(string arquivo)
     case 1:
         robo = new Robby(posIni, lab, maxSteps);
         break;
+
     case 2:
         robo = new ArTur(posIni, lab, maxSteps);
         break;
+
     case 3:
-        cout << "Robo 3" << endl;
-        //robo = new T8000();
+        robo = new T800(posIni, lab, maxSteps);
         break;
+
+    default:
+        robo = new Robby(posIni, lab, maxSteps);
     }
 
     GL::setMazeRobot(lab, robo);
